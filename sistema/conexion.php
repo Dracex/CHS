5d57f92203f78897f90ed3332eddb1312f2a37a1
Christian <christianjroche@gmail.com>
2017-02-22 17:51:33 +0100

    Prueba

--- sistema/conexion.php
+++ sistema/conexion.php
@@ -1,11 +1,11 @@
 <?php
 
 	function conectar () {
-		$username = "u155614104_db";
-		$password = "9lRT+mX185Kg";
+//		$username = "u155614104_db";
+//		$password = "9lRT+mX185Kg";
 		try {
-//			$conn = new PDO ('mysql:dbname=chs;host=localhost', "christian", "U8ETuqUW");
-			$conn = new PDO ('mysql:dbname=u155614104_chs;host=mysql.hostinger.es', $username, $password);
+			$conn = new PDO ('mysql:dbname=chs;host=localhost', "christian", "U8ETuqUW");
+//			$conn = new PDO ('mysql:dbname=u155614104_chs;host=mysql.hostinger.es', $username, $password);
 		} catch (Exception $errorConexion) {
 			echo "ERROR: $errorConexion";
 		}
