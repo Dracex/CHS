302e4016dfeaa3222e1f16c0e639ca7b13d4ca54
Christian <christianjroche@gmail.com>
2017-02-23 16:43:33 +0100

    FPEX

--- fpex2/fpex2.js
+++ fpex2/fpex2.js
@@ -103,12 +103,7 @@ $(document).ready(function () {
 
 
 function onSignIn(googleUser) {
-	var profile = googleUser.getBasicProfile();
-	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-	console.log('Name: ' + profile.getName());
-	console.log('Image URL: ' + profile.getImageUrl());
-	console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 	$(".g-signin2").hide();
 	$("#logout").show();
-	console.log("Connected");
+	console.log("User signed in");
 }
