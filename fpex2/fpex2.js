091daa704d5b553c1bfa64381dd2a3aa34e001e5
Christian <christianjroche@gmail.com>
2017-02-23 16:33:13 +0100

    FPEX

--- fpex2/fpex2.js
+++ fpex2/fpex2.js
@@ -85,6 +85,8 @@ $(document).ready(function () {
 		var auth2 = gapi.auth2.getAuthInstance();
 		auth2.signOut().then(function () {
 			console.log('User signed out.');
+			$(".g-signin2").show();
+			$(".signOut").hide();
 		});
 	});
 
