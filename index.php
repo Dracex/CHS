f2b0d4e929ee97186d01b7328ffc8869e3bd6718
Christian <christianjroche@gmail.com>
2017-03-14 19:12:42 +0100

    Prueba!

--- index.php
+++ index.php
@@ -184,13 +184,13 @@
 				</div>
 			</footer>
 		</div>
-		<form method="POST" action="http://caminahaciasantiago.esy.es/">
+		<form method="POST">
 			<input type="hidden" value="OK" name="err">
 			<input type="submit" value="send">
 		</form>
 		<?php
 				if (isset ($_POST['err'])) {
-					echo "Hola!"
+					echo "Hola!";
 				}
 			if (isset ($_SESSION['err'])) {
 				$err = $_SESSION['err'];
