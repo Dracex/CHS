9c8d4474014f0ded2e5888999ff5ca8db471afab
Christian <christianjroche@gmail.com>
2017-03-14 18:58:27 +0100

    Prueba!

--- index.php
+++ index.php
@@ -184,23 +184,18 @@
 				</div>
 			</footer>
 		</div>
-		<form method="POST" action="<?=$PHP_SELF?>">
+		<form method="POST">
 			<input type="hidden" value="OK" name="err">
 			<input type="submit" value="send">
 		</form>
 		<?php
-			if (isset($_POST['err'])) {
-				$err = $_POST['err'];
+			if (isset ($_POST['err'])) {
+				echo 'alert("Funciona sin action");';
 			}
 			if (isset ($_SESSION['err'])) {
 				$err = $_SESSION['err'];
 				echo "<script>";
 				switch ($err) {
-					case 'OK':
-						echo 'alert("Funciona sin action");';
-						$_SESSION['err'] = "";
-						break;
-					
 					case 'user':
 						echo 'alert("Ya hay un usuario correo");';
 						$_SESSION['err'] = "";
